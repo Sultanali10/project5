43a3465c6eb75cd18c2bc9f53545607740863dd0
Sultan Ali <sultanjamali1986@gmail.com>
2020-06-09 23:03:47 +0300

    started file

--- project5/ViewController.swift
+++ project5/ViewController.swift
@@ -8,11 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        if let startURL = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            if let startURLContent = try? String(contentsOf: startURL){
+                allWords = startURLContent.components(separatedBy: "\n")
+            }
+        }
+        if allWords.isEmpty {
+            allWords = ["Empty because no file in bundle"]
+        }
+        
+        title = allWords.randomElement()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
     }
 
 
