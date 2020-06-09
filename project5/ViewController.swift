86cac351b4575737ba7ecb5484092fe38aeeede4
Sultan Ali <sultanjamali1986@gmail.com>
2020-06-09 22:11:40 +0300

    Initial Commit

---
+++ project5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project5
+//
+//  Created by Sultan Ali on 09/06/2020.
+//  Copyright © 2020 Sultan Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
